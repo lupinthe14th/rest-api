41d1388ce9d85850077c353b016465d17573cd65
Hideo Suzuki <lupinthe14th@gmail.com>
2018-02-04 04:17:02 +0900

    [add] refactoring

---
+++ lookup/api/v1/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func NewRoutes() (*rest.Api, error) {
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/mxrecords/#host", MxRecord),
+	)
+	if err != nil {
+		return api, err
+	}
+	api.SetApp(router)
+	return api, nil
+}
